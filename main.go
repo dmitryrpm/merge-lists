948040d429f62986881cf6831eeb9b89f0302dc0
dmtikhonov <dmtikhonov@ozon.ru>
2022-08-22 14:53:49 +0300

    impl: linked list merger

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+// Вам даны "головы" двух отсортированных по возрастанию связанных списков l1 и l2.
+// Объедините два списка в один отсортированный список.
+// Список должен быть составлен путем соединения вместе узлов первых двух списков.
+
+// Результат – "голова" объединенного связанного списка.
+
+// Пример:
+// l1: 2 -> 3 -> 5
+// l2: 1 -> 3 -> 5
+
+// Ожидаемый результат:
+// 1 -> 2 -> 3 -> 3 -> 5 -> 5
+
+// ListNode Узел списка выглядит следующим образом
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// main run me
+func main() {
+	l1 := &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 5}}}
+	l2 := &ListNode{Val: 1, Next: &ListNode{Val: 3, Next: &ListNode{Val: 5}}}
+	l3 := mergeTwoLists(l1, l2)
+	for l3 != nil {
+		if l3.Next != nil {
+			print(l3.Val, " -> ")
+		} else {
+			print(l3.Val)
+		}
+		l3 = l3.Next
+	}
+}
+
+// mergeTwoLists merge 2 linked lists
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil && l2 == nil {
+		return nil
+	}
+
+	var tmp = new(ListNode)
+	var cursor = tmp
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			cursor.Next = l1
+			l1 = l1.Next
+		} else {
+			cursor.Next = l2
+			l2 = l2.Next
+		}
+		// next step
+		cursor = cursor.Next
+	}
+
+	// last
+	if l1 != nil {
+		cursor.Next = l1
+	} else {
+		cursor.Next = l2
+	}
+
+	return tmp.Next
+}
